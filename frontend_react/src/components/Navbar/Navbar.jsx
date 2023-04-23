78a021cfdf7547087a4f26f9663e2eec8be1983e
Tristan Jagan <tristan.jgn@gmail.com>
2023-04-23 00:29:17 -0400

    adds navbar structure

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -2,13 +2,19 @@ import "./Navbar.scss";
 
 function Navbar() {
   return (
-    <nav>
-      <div>
-        
-      </div>
+    <nav className="navbar">
+      <ul className="navbar__list">
+        {["home", "about", "skills", "projects", "contact"].map((item) => (
+          <li key={`link-${item}`} className="navbar__item">
+            <div className="navbar__dot" />
+            <a className="navbar__link" href={`#${item}`}>
+              {item}
+            </a>
+          </li>
+        ))}
+      </ul>
     </nav>
-  
-  )
+  );
 }
 
 export default Navbar;
