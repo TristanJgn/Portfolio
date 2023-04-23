ac901a0c25efb31e70b86a6d3085a872b74a2c56
Tristan Jagan <tristan.jgn@gmail.com>
2023-04-23 01:08:53 -0400

    adds hamburger menu for mobile view

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 import "./Navbar.scss";
 
 function Navbar() {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="navbar">
       <ul className="navbar__list">
@@ -13,6 +18,41 @@ function Navbar() {
           </li>
         ))}
       </ul>
+
+      <div className="navbar-menu">
+        <HiMenuAlt4
+          className="navbar-menu__icon"
+          onClick={() => setToggle(true)}
+        />
+
+        {toggle && (
+          <motion.div
+            className="navbar-menu__container"
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX
+              className="navbar-menu__icon navbar-menu__icon--close"
+              onClick={() => setToggle(false)}
+            />
+            <ul className="navbar-menu__list">
+              {["home", "about", "skills", "projects", "contact"].map(
+                (item) => (
+                  <li key={item} className="navbar-menu__item">
+                    <a
+                      className="navbar-menu__link"
+                      href={`#${item}`}
+                      onClick={() => setToggle(false)}
+                    >
+                      {item}
+                    </a>
+                  </li>
+                )
+              )}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 }
