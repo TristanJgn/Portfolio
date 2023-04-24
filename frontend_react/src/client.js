83f8b3197c9997f778638fb0eb1c1640a0b911c2
Tristan Jagan <tristan.jgn@gmail.com>
2023-04-24 03:09:19 -0400

    adds configuration of connection between react app and sanity client

---
+++ frontend_react/src/client.js
@@ -0,0 +1,15 @@
+import { SanityClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+// Connect react app to the sanity project
+export const client = SanityClient({
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: "production",
+  apiVersion: "2023-04-24",
+  useCdn: true,
+  token: process.env.REACT_APP_SANITY_TOKEN,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
